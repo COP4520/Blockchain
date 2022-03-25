fa31858a6f09b03beb8e4f053601f08f05a3633f
DahliaL <dahlia.lapom@gmail.com>
2022-03-25 15:19:52 -0400

    adding v2

---
+++ v2/src/main/java/Block.java
@@ -0,0 +1,44 @@
+import com.google.common.hash.Hashing;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Block {
+    ArrayList<Transaction> transactions;
+    Timestamp timestamp;
+    String previousHash;
+    String hash;
+    int nonce;
+
+    public Block(ArrayList<Transaction> transactions, Timestamp timestamp){
+        this(transactions, timestamp, "");
+    }
+
+    public Block(ArrayList<Transaction> transactions, Timestamp timestamp, String previousHash){
+        this.transactions = transactions;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        nonce = 0;
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash(){
+        String json = new JSONArray(this.transactions).toString();
+        return Hashing.sha256()
+                .hashString(this.previousHash + this.timestamp + json + this.nonce, StandardCharsets.UTF_8)
+                .toString();
+    }
+
+    public void mineBlock(int difficulty){
+        long start = System.nanoTime();
+        while (!this.hash.substring(0, difficulty).equals(StringUtils.leftPad("", difficulty, "0"))) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        long end = System.nanoTime();
+        double ms = ((double)end-start) / 1000000;
+        System.out.printf("Block Mined in %.2fms with mining hash %s%n", ms, this.hash);
+    }
+}
---
+++ v2/src/main/java/Blockchain.java
@@ -0,0 +1,57 @@
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Blockchain {
+    ArrayList<Block> chain;
+    ArrayList<Transaction> pendingTransactions;
+    int difficulty;
+    int miningReward;
+
+    public Blockchain() {
+        this.chain = new ArrayList<>();
+        this.chain.add(this.createGenesisBlock());
+        this.pendingTransactions = new ArrayList<>();
+        this.difficulty = 3;
+        this.miningReward = 100;
+    }
+
+    public Block createGenesisBlock() {
+        return new Block(new ArrayList<>(), new Timestamp(System.currentTimeMillis()), "0");
+    }
+
+    public Block getLatestBlock() {
+        // Blockchain guaranteed to have a minimum size of 1 containing the genesis block
+        return chain.get(this.chain.size() - 1);
+    }
+
+    public void minePendingTransactions(String rewardAddress) {
+        Transaction reward = new Transaction(null, rewardAddress, this.miningReward);
+        this.pendingTransactions.add(reward);
+
+        Block block = new Block(this.pendingTransactions, new Timestamp(System.currentTimeMillis()), this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        this.chain.add(block);
+        this.pendingTransactions.clear();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        // TODO ensure transaction is valid and the balance of wallet before accepting transaction
+
+        this.pendingTransactions.add(transaction);
+        System.out.println("Added transaction to pending transactions.");
+    }
+
+    public boolean isChainValid() {
+        for(int i = 1; i < chain.size(); i++) {
+            Block currentBlock = this.chain.get(i);
+            Block previousBlock = this.chain.get(i-1);
+
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()))
+                return false;
+            if(!previousBlock.hash.equals(previousBlock.calculateHash()))
+                return false;
+        }
+        return true;
+    }
+}
