a8bfeba1c3324bb1efbc1001725b4430ff0ec90d
weiiyii <cwy.sophiechen@gmail.com>
2022-04-02 21:17:36 -0400

    modify TCP server code

--- v2_Proof_of_Stake/src/main/java/Main.java
+++ v2_Proof_of_Stake/src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class Main {
     public static void main(String[] args) {
 //        Block block = new Block(new ArrayList<>(), new Timestamp(System.currentTimeMillis()));
 //        block.mineBlock(5);
 
         Server server = new Server(9000);
+
         Blockchain blockchain = new Blockchain();
         blockchain.addTransaction(new Transaction("abc123", "def456", 10));
         blockchain.addTransaction(new Transaction("abc123", "def456", 20));
--- v2_Proof_of_Stake/src/main/java/Server.java
+++ v2_Proof_of_Stake/src/main/java/Server.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.io.*;
-import java.sql.Timestamp;
 
 public class Server {
     
@@ -8,65 +7,64 @@ public class Server {
     private Socket          socket   = null;
     private ServerSocket    server   = null;
     private DataInputStream in       =  null;
-    private int balance = 0;
  
     // constructor with port
     public Server(int port)
     {
         // starts server and waits for a connection
-        try
-        {
+        try{
             server = new ServerSocket(port);
+            
             System.out.println("Server started");
- 
             System.out.println("Waiting for a client...");
-        }
-        catch(IOException i)
-        {
+
+            socket = server.accept();
+
+        }catch(IOException i){
+
             System.out.println(i);
         }
-    }
-
-    //handle server connection
-    public void handleConn(){
-
-       
-        /**
-         * TODO:
-         * Receive a broadcast of the latest blockchain
-         * Receive a broadcast of which validator in the network won the latest block
-         * Add itself to the overall list of validators
-         * Propose a new block
-         * 
-         */
-
-        // FIXME: Need an infinite loop
-        socket = server.accept();
-
-         // takes input from the client socket
-         in = new DataInputStream(
-            new BufferedInputStream(socket.getInputStream()));
-
+        
         String line = "";
-        
-        try
-        {
-            line = in.readUTF();
 
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
+        while(true) {
+            try{
 
-        Timestamp ts = new Timestamp(System.currentTimeMillis());
-        
-        
-        System.out.println("Closing connection");
+                System.out.print("Enter detail about transaction: ");
+                // takes input from the client socket
+                in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+ 
+                try
+                {
+                    line = in.readUTF();
+                    if(line.equals("Over")){
+                        break;
+                    }
+                    // TODO: Create a new block, etc.
+                    System.out.println(line);
+                    
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+
+            } catch(IOException i) {
+                
+                System.out.println(i);
+            }
+            
+        }
 
         // close connection
-        socket.close();
-        in.close();
+        try{
+            socket.close();
+            in.close();
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+        
     }
 
 }
