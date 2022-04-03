ca563a0d023eb69ff910d9398492a7edfa75e754
weiiyii <cwy.sophiechen@gmail.com>
2022-04-02 21:45:49 -0400

    TCP server code

--- v2_Proof_of_Stake/src/main/java/Server.java
+++ v2_Proof_of_Stake/src/main/java/Server.java
@@ -4,9 +4,9 @@ import java.io.*;
 public class Server {
     
     //initialize socket and input stream
-    private Socket          socket   = null;
-    private ServerSocket    server   = null;
-    private DataInputStream in       =  null;
+    public Socket          socket   = null;
+    public ServerSocket    server   = null;
+    public DataInputStream in       =  null;
  
     // constructor with port
     public Server(int port)
@@ -18,50 +18,8 @@ public class Server {
             System.out.println("Server started");
             System.out.println("Waiting for a client...");
 
-            socket = server.accept();
-
         }catch(IOException i){
 
-            System.out.println(i);
-        }
-        
-        String line = "";
-
-        while(true) {
-            try{
-
-                System.out.print("Enter detail about transaction: ");
-                // takes input from the client socket
-                in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
- 
-                try
-                {
-                    line = in.readUTF();
-                    if(line.equals("Over")){
-                        break;
-                    }
-                    // TODO: Create a new block, etc.
-                    System.out.println(line);
-                    
-                }
-                catch(IOException i)
-                {
-                    System.out.println(i);
-                }
-
-            } catch(IOException i) {
-                
-                System.out.println(i);
-            }
-            
-        }
-
-        // close connection
-        try{
-            socket.close();
-            in.close();
-        } catch(IOException i) {
             System.out.println(i);
         }
         
