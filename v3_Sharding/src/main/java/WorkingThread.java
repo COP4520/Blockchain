57e8520813350ad5a8d17f62254491f3943e06c4
Ster <sterz.dev@gmail.com>
2022-04-28 10:42:17 -0400

    Handle client disconnects

--- v3_Sharding/src/main/java/WorkingThread.java
+++ v3_Sharding/src/main/java/WorkingThread.java
@@ -25,43 +25,47 @@ public class WorkingThread extends Thread {
                 new BufferedInputStream(socket.getInputStream()));
 
             out  = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            System.out.println(e);
-        }
 
-        String line = "";
-        while(!line.equals("Over")) {
-            try
-            {
-                line = in.readUTF();
+            String line = "";
+            while(true) {
+                try
+                {
+                    line = in.readUTF();
 
-                if(line.equals("Over")){
+                    if(line.equals("Over")){
+                        break;
+                    }
+
+                    String [] data = line.split("\\s+");
+                    if(data.length < 2){
+                        System.out.print("Wrong input");
+                        continue;
+                    }
+                    String address = data[0];
+                    int stake = Integer.parseInt(data[1]);
+
+                    System.out.println("Working thread " + Thread.currentThread().getId() + " with stake = " + stake);
+
+                    if(stake<10) {
+                        out.writeUTF("Request ignored: Stake must be at least 10");
+                    }
+                    else {
+                        out.writeUTF("Stake " + stake + " added to validator list");
+                        server.blockchain.addValidator(address, stake);
+                    }
+
+                }
+                catch(IOException i)
+                {
                     break;
                 }
-
-                String [] data = line.split("\\s+");
-                if(data.length < 2){
-                    System.out.print("Wrong input");
-                    continue;
-                }
-                String address = data[0];
-                int stake = Integer.parseInt(data[1]);
-
-                System.out.println("Working thread " + Thread.currentThread().getId() + " with stake = " + stake);
-
-                if(stake<10) {
-                    out.writeUTF("Request ignored: Stake must be at least 10");
-                }
-                else {
-                    out.writeUTF("Stake " + stake + " added to validator list");
-                    server.blockchain.addValidator(address, stake);
-                }
-                
             }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
+            in.close();
+            this.socket.close();
+            System.out.println("Client " + Thread.currentThread().getId() + " disconnected.");
+
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 }
